f29ef3d19612a76597e727c8b0b9cc160b919ed1
takapg <tkpgacc@gmail.com>
2023-03-10 01:29:33 +0900

    ci: Add GitHub Actions (wip)

---
+++ sandbox/scripts/setup_localstack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+alias aws="aws --endpoint-url http://${LOCALSTACK_HOSTNAME}:4566"
+BUCKET_NAME=localstack-tfstate
+
+aws s3api create-bucket \
+    --bucket ${BUCKET_NAME} \
+    --create-bucket-configuration LocationConstraint=ap-northeast-1
+
+aws s3api put-bucket-versioning \
+    --bucket ${BUCKET_NAME} \
+    --versioning-configuration Status=Enabled
+
+aws s3api put-public-access-block \
+    --bucket ${BUCKET_NAME} \
+    --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
+
+aws s3api put-bucket-encryption \
+    --bucket ${BUCKET_NAME} \
+    --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
